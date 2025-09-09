3b91a70026a7f838123d37639685d2ef67b8535d
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-09-09 16:30:47 +0900

    logback 적용

--- api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,6 +1,10 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +18,26 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     //http://localhost:8080/api/v1/get-api/hello
     @RequestMapping(value="/hello", method= RequestMethod.GET)
     public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다");
         return "Hello World";
     }
 
     //http://localhost:8080/api/v1/get-api/name
     @GetMapping(value = "/name")
     public String getName() {
+        LOGGER.info("getName 메서드가 호출되었습니다");
         return "Flature";
     }
 
     // http://localhost:8080/api/v1/get-api/variable1/{String 값}
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {}", variable);
         return variable;
     }
 
@@ -39,11 +48,12 @@ public class GetController {
     }
 
     // http://localhost:8080/api/v1/get-api/request1?name=value1&email=value2&organization=value3
+    @Operation(summary = "GET 메서드 예제", description = "@RequestParam을 활용한 GET Method")
     @GetMapping(value="/request1")
     public String getRequestParam1(
-            @RequestParam String name,
-            @RequestParam String email,
-            @RequestParam String organization) {
+            @Parameter(description = "이름", required = true) @RequestParam String name,
+            @Parameter(description = "이메일", required = true)@RequestParam String email,
+            @Parameter(description = "회사", required = true)@RequestParam String organization) {
         return name + " " + email + " " + organization;
     }
 
