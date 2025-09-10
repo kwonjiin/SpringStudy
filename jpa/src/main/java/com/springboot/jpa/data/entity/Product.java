f88b4d1122caf7d2957d291a9e278b3226e33b22
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-09-10 15:29:31 +0900

    6.10 DAO 연동을 위한 컨트롤러와 서비스 설계

--- jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
+++ jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -6,20 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name="product")
-@Getter
-@Setter
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long numer;
+    private Long number;
 
     @Column(nullable = false)
     private String name;
@@ -35,4 +31,52 @@ public class Product {
     private LocalDateTime updatedAt;
 
 
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
