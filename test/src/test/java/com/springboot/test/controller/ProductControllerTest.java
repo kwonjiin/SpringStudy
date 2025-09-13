e8392d06aef4026a89e2f8ab0a25fd7e0a2e4fcc
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-09-13 23:54:54 +0900

    create 테스트코드 작성중

--- test/src/test/java/com/springboot/test/controller/ProductControllerTest.java
+++ test/src/test/java/com/springboot/test/controller/ProductControllerTest.java
@@ -1,5 +1,6 @@
 package com.springboot.test.controller;
 
+import com.springboot.test.data.dto.ProductDto;
 import com.springboot.test.data.dto.ProductResponseDto;
 import com.springboot.test.service.impl.ProductServiceImpl;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -33,11 +36,32 @@ public class ProductControllerTest {
 
         String productId = "123";
 
-//        mockMvc.perform(
-//                get("/product?number=" + productId))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.number").exists())
-//                .andExpect(jsonPath())
+        mockMvc.perform(
+                get("/product?number=" + productId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.number").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andDo(print());
+
+        verify(productService).getProduct(123L);
     }
+
+//    @Test
+//    @DisplayName("Product 데이터 생성 테스트")
+//    void createProductTest() throws Exception {
+//        // Mock 객체에서 특정 메서드가 실행되는 경우 실제 Return을 줄 수 없기 때문에 아래와 같이 가정사항을 만들어줌
+//        given(productService.saveProduct(new ProductDto("pen", 5000, 2000)))
+//                .willReturn(new ProductResponseDto(12315L, "pen", 5000, 2000));
+//
+//        ProductDto productDto = ProductDto.builder()
+//                .name("pen")
+//                .price(5000)
+//                .stock(2000)
+//                .build();
+//
+//
+//    }
 
 }
